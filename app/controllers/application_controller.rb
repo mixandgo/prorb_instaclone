56a5d0074f31951941b2bd22aead729e06efacac
Cezar Halmagean <cezar@mixandgo.com>
2022-07-29 13:04:36 +0300

    Added username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
