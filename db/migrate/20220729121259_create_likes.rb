2f28e7f02f8cab1e1c07365a643bc458594422d6
Cezar Halmagean <cezar@mixandgo.com>
2022-08-04 10:49:59 +0300

    Added Like model

---
+++ db/migrate/20220729121259_create_likes.rb
@@ -0,0 +1,8 @@
+class CreateLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :users, :posts, table_name: :likes do |t|
+      t.index :user_id
+      t.index :post_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_135311) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_29_121259) do
+  create_table "likes", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "body"
