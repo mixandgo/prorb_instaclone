1f7363b9ddc68da803e38ea075098afcc08299c3
Cezar Halmagean <cezar@mixandgo.com>
2022-08-09 13:56:20 +0300

    Added unlikes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,4 +3,9 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     current_user.like!(@post)
   end
+
+  def destroy
+    @post = current_user.posts.find_by(id: params[:post_id])
+    current_user.likes.where(post_id: @post.id).delete_all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :posts, only: [:create, :new] do
-    resources :likes, only: :create
+    resources :likes, only: :create do
+      collection do
+        delete :destroy
+      end
+    end
   end
   devise_for :users
   root "site#index"
